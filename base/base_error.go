446f36e3d494bd50d7b9d49ee6f451902e7b90da
zhangkai <zhangkai.gis@163.com>
2019-04-25 13:43:58 +0800

    modify error check when error is nil tell user the location

--- base/base_error.go
+++ base/base_error.go
@@ -12,9 +12,6 @@ import (
 //
 // This code may not conform to popular Go coding idioms
 func where_am_i(err error, depthList ...int) string {
-	if err == nil {
-		return ""
-	}
 	var depth int
 	if depthList == nil {
 		depth = 1
@@ -22,7 +19,15 @@ func where_am_i(err error, depthList ...int) string {
 		depth = depthList[0]
 	}
 	function, file, line, _ := runtime.Caller(depth)
-	return fmt.Sprintf("Error %s File: %s  Function: %s Line: %d", err.Error(), file, runtime.FuncForPC(function).Name(), line)
+
+	tips := ""
+	if err != nil {
+		tips = fmt.Sprintf("Error %s File: %s  Function: %s Line: %d", err.Error(), file, runtime.FuncForPC(function).Name(), line)
+	} else {
+		tips = fmt.Sprintf("Error %s File: %s  Function: %s Line: %d", "no error", file, runtime.FuncForPC(function).Name(), line)
+	}
+
+	return tips
 }
 
 func ErrorCheck(err error) {
