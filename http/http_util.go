3a2fdccb07f34633232063c07bca14b4f8d50c9c
zhangkai <zhangkai.gis@163.com>
2019-07-17 18:14:11 +0800

    add http utils

--- http/http_util.go
+++ http/http_util.go
@@ -1,9 +1,15 @@
 package http
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/giskook/go/base"
+	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
 )
 
 func GetClientIP(req *http.Request) string {
@@ -14,4 +20,47 @@ func GetClientIP(req *http.Request) string {
 	}
 
 	return string(net.ParseIP(ip))
+}
+
+type Response struct {
+	Code string      `json:"code"`
+	Desc string      `json:"desc"`
+	Data interface{} `json:"data,omitempty"`
+}
+
+func EncodeResponse(w http.ResponseWriter, code string, data interface{}, errmsg string) {
+	gr := &Response{
+		Code: code,
+		Desc: errmsg,
+		Data: data,
+	}
+	MarshalJson(w, gr)
+}
+
+func RecordReq(r *http.Request) {
+	v, e := httputil.DumpRequest(r, true)
+	if e != nil {
+		return
+	}
+	log.Println(string(v))
+}
+
+// MarshalJson 把对象以json格式放到response中
+func MarshalJson(w http.ResponseWriter, v interface{}) error {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	fmt.Fprint(w, string(data))
+	return nil
+}
+
+// UnMarshalJson 从request中取出对象
+func UnMarshalJson(req *http.Request, v interface{}) error {
+	result, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal([]byte(bytes.NewBuffer(result).String()), v)
+	return nil
 }
